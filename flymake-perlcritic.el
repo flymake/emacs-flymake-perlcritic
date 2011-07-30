a5eb24d905567b781654944914d66b221a58f04f
Sam Graham <git@illusori.co.uk>
2011-07-30 08:55:37 +0100

    Wrapped in eval-after-load for safer run-order resolution with flymake.el. Made conditional use of original/my-patched flymake temp file creation. Updated README.

--- flymake-perlcritic.el
+++ flymake-perlcritic.el
@@ -24,27 +24,28 @@
   :group 'flymake-perlcritic
   :type 'integer)
 
-;; Add a new error pattern to catch Perl::Critic output, this is a custom format
-;; defined in perlcritic_flymake since the Perl::Critic default isn't parsable in
-;; a way that flymake.el likes.
 (eval-after-load "flymake"
-  '(add-to-list 'flymake-err-line-patterns
-                '("\\(.*\\):\\([0-9]+\\):\\([0-9]+\\): \\(.*\\)" 1 2 3 4))
+  '(progn
+    ;; Add a new error pattern to catch Perl::Critic output, this is a custom format
+    ;; defined in perlcritic_flymake since the Perl::Critic default isn't parsable in
+    ;; a way that flymake.el likes.
+    (add-to-list 'flymake-err-line-patterns
+                 '("\\(.*\\):\\([0-9]+\\):\\([0-9]+\\): \\(.*\\)" 1 2 3 4))
+    (defun flymake-perl-init ()
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                          (if (fboundp 'flymake-create-temp-copy)
+                            'flymake-create-temp-copy
+                            'flymake-create-temp-inplace)))
+             (local-file (file-relative-name temp-file
+                           (file-name-directory buffer-file-name))))
+        (list flymake-perlcritic-command
+          (list local-file
+            (concat
+              (if flymake-perlcritic-profile (concat "--profile " flymake-perlcritic-profile) "")
+              (concat "--severity " (number-to-string flymake-perlcritic-severity))))))
+      )
+    (add-hook 'perl-mode-hook (lambda() (flymake-mode 1)))
+    )
   )
-
-(defun flymake-perl-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-copy))
-         (local-file  (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list flymake-perlcritic-command
-      (list local-file
-        (concat
-          (if flymake-perlcritic-profile (concat "--profile " flymake-perlcritic-profile) "")
-          (concat "--severity " (number-to-string flymake-perlcritic-severity))))))
-  )
-
-(add-hook 'perl-mode-hook (lambda() (flymake-mode 1)))
 
 (provide 'flymake-perlcritic)
